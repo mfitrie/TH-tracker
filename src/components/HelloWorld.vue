d1a985d7af1238e38f3f522d2cd0c0223f4565fc
Fitrie <mfitrie78@gmail.com>
2023-04-21 00:39:02 +0800

    adding daisyui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,38 @@
 <template>
-    <div>
-      <ul>
-        <li v-for="item in food">
-          {{ item }}
-        </li>
-      </ul>
-      <button @click="addFood">Add carbonara</button>
+
+    <div class="navbar bg-primary">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl">TH Store Tracker</a>
+      </div>
+      <div class="flex-none">
+        <button class="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </button>
+      </div>
+    </div>
+    <div class="drawer">
+      <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <!-- Page content here -->
+        <div class="container h-screen m-auto">
+          <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+          <ul>
+            <li v-for="item in food">
+              {{ item }}
+            </li>
+          </ul>
+          <button class="btn btn-ghost" @click="addFood">Add carbonara</button>
+        </div>
+      </div> 
+      <div class="drawer-side">
+        <label for="my-drawer" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+          <!-- Sidebar content here -->
+          <li><a>Sidebar Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+          
+        </ul>
+      </div>
     </div>
   </template>
   
