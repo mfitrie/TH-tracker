c915707b9a5fcfca0ef5bce97e65081f8bf2fcf7
Fitrie <mfitrie78@gmail.com>
2023-04-21 00:03:15 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="item in food">
+          {{ item }}
+        </li>
+      </ul>
+      <button @click="addFood">Add carbonara</button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+    import {ref} from 'vue';
+    const food = ref<Array<string>>(["Pizza", "French fries", "Spaghetti"]);
+    
+    const addFood = () => {
+        food.value.push('carboonara');
+    }
+    
+  </script>
