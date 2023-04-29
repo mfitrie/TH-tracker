54bddc66f0519c98547355f9050d6d2545c9ed3c
Fitrie <mfitrie78@gmail.com>
2023-04-29 11:58:30 +0800

    change table to regular tailwind

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,52 +1,43 @@
 <template>
-    <div class="overflow-x-auto">
-        <!-- <h5>Click emojis to remove them.</h5>
-        <ul v-auto-animate>
-            <li
-            v-for="item in items"
-            :key="item"
-            @click="removeItem(item)"
-            >
-            {{ item }}
-            </li>
-        </ul> -->
-        <table class="table table-zebra w-full">
-            <thead>
-                <tr>
-                    <th></th> 
-                    <th>User ID</th> 
-                    <th>ID</th> 
-                    <th>Title</th> 
-                    <th>Body</th> 
-                    <th></th> 
-                    <th></th> 
-                </tr>
-            </thead> 
-            <tbody v-auto-animate>
-                <tr class="hover" v-for="({ userId, id, title, body }, index) in listData">
-                    <th>{{ index + 1 }}</th> 
-                    <td>{{ userId }}</td> 
-                    <td>{{ id  }}</td> 
-                    <td>{{ title }}</td> 
-                    <td>{{ body }}</td> 
-                    <td>
-                        <button class="btn btn-outline btn-info">Edit</button>
-                    </td>
-                    <td>
-                        <button class="btn btn-outline btn-error" @click="deleteItem(id)">Delete</button>
-                    </td>  
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th></th> 
-                    <th>User ID</th> 
-                    <th>ID</th> 
-                    <th>Title</th> 
-                    <th>Body</th> 
-                    <th></th> 
-                </tr>
-            </tfoot>
+    <div class="px-5">
+        <table class="table-auto">
+        <thead class="h-12">
+            <tr>
+                <th>User ID</th>
+                <th>ID</th> 
+                <th>Title</th> 
+                <th>Body</th>
+            </tr>
+        </thead>
+        <tbody v-auto-animate>
+            <tr class="hover" v-for="({ userId, id, title, body }, index) in listData">
+                <td class="text-center">{{ userId }}</td> 
+                <td class="text-center">{{ id  }}</td> 
+                <td class="">{{ title }}</td> 
+                <td class="">{{ body }}</td> 
+                <td class="pr-3">
+                    <button class="btn btn-sm btn-outline btn-info">Edit</button>
+                </td>
+                <td class="">
+                    <button class="btn btn-sm btn-outline btn-error" @click="deleteItem(id)">Delete</button>
+                </td>  
+            </tr>
+            <!-- <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+            </tr>
+            <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+            </tr>
+            <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+            </tr> -->
+        </tbody>
         </table>
     </div>
 </template>
