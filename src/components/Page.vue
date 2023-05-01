87f159c6331b75548c485a70a2b03bae73b01e38
Fitrie <mfitrie78@gmail.com>
2023-05-01 16:33:56 +0800

    add search function

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="px-5">
-        <div class="p-5 flex justify-end">
-            <button class="btn btn-sm btn-outline btn-info" @click="addItem">Add Item</button>
+        <div class="py-5 flex items-center justify-between">
+            <div class="w-1/4">
+                <input class="input input-bordered w-full max-w-sm h-9" v-model="searchData" type="text" placeholder="Type here" />
+            </div>
+            <button class="btn btn-sm btn-outline btn-info justify-self-end;" @click="addItem">Add Item</button>
         </div>
         <table class="table-auto">
             <thead class="h-12">
@@ -13,7 +16,7 @@
                 </tr>
             </thead>
             <tbody v-auto-animate>
-                <tr v-for="({ userId, id, title, body }, index) in listData">
+                <tr v-for="({ userId, id, title, body }, index) in searchListData">
                     <td class="p-2 text-center">{{ userId }}</td> 
                     <td class="p-2 text-center">{{ id  }}</td> 
                     <td class="p-2">{{ title }}</td> 
@@ -30,7 +33,7 @@
     </div>
 </template>
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { ref, watch, computed } from 'vue';
     import autoAnimate from '@formkit/auto-animate';
 
     const props = defineProps({
@@ -42,9 +45,24 @@
         }>
     });
 
-    
-
     const listData = ref(props.listData);
+    const searchData = ref('');
+
+    const searchListData = computed(() => {
+        if(searchData.value.length === 0){
+            return listData.value;
+        }
+
+        return listData.value.filter((item) => (item.title.includes(searchData.value.toLowerCase())))
+    });
+
+    watch(searchData, (value: string) => {
+        if(value.length === 0){
+            return;
+        }
+
+        const searchResult = listData.value.filter((item) => (item.title.includes(value.toLowerCase()))).map(item => item.title);
+    });
 
 
     function addItem(){
